8a0e930aa862d913566930428c0b751411a28ce7
Demivdm <demi.van.der.maarl@hva.nl>
2023-10-26 12:19:29 +0200

    Update first-page.js

--- platform-grote-themas/src/components/HomepageFeatures/first-page.js
+++ platform-grote-themas/src/components/HomepageFeatures/first-page.js
@@ -1,8 +1,24 @@
-import React from "react";
-import Layout from "@theme/Layout";
-import { firstPost } from ".";
+// import React from "react";
+// import Layout from "@theme/Layout";
+// import { firstPost } from ".";
 
-// Example using the WordPress REST API
+// fetch("http://localhost/wp-headless/server/wordpress/wp-json/wp/v2/posts")
+//   .then((response) => response.json())
+//   .then((data) => {})
+//   .catch((error) =>
+//     console.error("Error fetching data from WordPress:", error)
+//   );
+
+// export function Post() {
+//   return (
+//     <Layout>
+//       <h1>Werkvormen{data.posts.slug}</h1>
+//       <p>Dit is een werkvorm</p>
+//     </Layout>
+//   );
+// }
+// export default firstPost;
+
 fetch("http://localhost/wp-headless/server/wordpress/wp-json/wp/v2/posts")
   .then((response) => response.json())
   .then((data) => {})
@@ -10,12 +26,15 @@ fetch("http://localhost/wp-headless/server/wordpress/wp-json/wp/v2/posts")
     console.error("Error fetching data from WordPress:", error)
   );
 
-export function Post() {
-  return (
-    <Layout>
-      <h1>Werkvormen{data.posts.slug}</h1>
-      <p>Dit is een werkvorm</p>
-    </Layout>
-  );
+import React, { Component } from "react";
+
+export class werkvormen extends Component {
+  state = {
+    posts: [],
+    isLoaded: false,
+  };
+  render() {
+    return <div></div>;
+  }
 }
-export default firstPost;
+export default werkvormen;
